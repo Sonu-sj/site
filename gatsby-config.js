cb3635c752ec1f4eb4916ac98dc5044ece99d983
JSONUSAM <jsonusam@ford.com>
2021-08-02 11:15:07 +0530

    correct

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: 'https://cmspoc.herokuapp.com',
         collectionTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'fleet-owners'
         ],
